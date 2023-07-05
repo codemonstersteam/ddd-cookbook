39b12a274146e268ee9a2333ef5d83c757b12a60
huggybear <maxim@codemonsters.team>
2023-07-05 14:40:06 +0300

    Пофиксен баг

--- src/main/kotlin/team/codemonsters/ddd/toolkit/domain/common/MonoExtensions.kt
+++ src/main/kotlin/team/codemonsters/ddd/toolkit/domain/common/MonoExtensions.kt
@@ -1,14 +1,15 @@
 package team.codemonsters.ddd.toolkit.domain.common
 
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 
 fun <T, R> Mono<Result<T>>.pipe(function: (T) -> Mono<Result<R>>)
         : Mono<Result<R>> =
     this.flatMap { result ->
         result.fold(
             { value -> function(value) },
-            { exception -> Result.failure<R>(exception) }
-        ) as Mono<out Result<R>>?
+            { exception -> Result.failure<R>(exception).toMono() }
+        )
     }
 
 fun <T, R> Mono<Result<T>>.mapResult(function: (T) -> Result<R>)
